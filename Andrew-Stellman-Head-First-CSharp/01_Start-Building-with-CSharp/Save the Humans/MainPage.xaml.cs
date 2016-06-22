8c5902fa47a4a1601ab6b8ac24144db44e693f53
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-22 03:39:27 +0300

    add StartGame method

--- Andrew-Stellman-Head-First-CSharp/01_Start-Building-with-CSharp/Save the Humans/MainPage.xaml.cs
+++ Andrew-Stellman-Head-First-CSharp/01_Start-Building-with-CSharp/Save the Humans/MainPage.xaml.cs
@@ -142,7 +142,20 @@ namespace Save_the_Humans
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            AddEnemy();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            human.IsHitTestVisible = true;
+            humanCaptured = false;
+            progressBar.Value = 0;
+            startButton.Visibility = Visibility.Collapsed;
+            playArea.Children.Clear();
+            playArea.Children.Add(target);
+            playArea.Children.Add(human);
+            enemyTimer.Start();
+            targetTimer.Start();
         }
 
         private void AddEnemy()
