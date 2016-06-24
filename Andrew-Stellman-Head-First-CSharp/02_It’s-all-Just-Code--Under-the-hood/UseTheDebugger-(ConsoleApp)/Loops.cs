12732113889eb1bd91ecd12dc97e192b1b6cbde5
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-24 20:07:41 +0300

    add For loop

--- Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/Loops.cs
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/Loops.cs
@@ -10,13 +10,27 @@ namespace UseTheDebugger__ConsoleApp_
     {
         public static void Looping()
         {
+            Console.WriteLine("------------");
             Console.WriteLine("Looping");
             WhileLoop();
+            ForLoop();
 
+        }
+
+        private static void ForLoop()
+        {
+            Console.WriteLine("----for-----");
+            int count = 0;
+            for (int i = 0; i < 8; i = i + 2)
+            {
+                count++;
+                Console.WriteLine($"Count {count} times");
+            }
         }
 
         private static void WhileLoop()
         {
+            Console.WriteLine("----while---");
             int x = 12;
             while (x > 5)
             {
@@ -24,6 +38,7 @@ namespace UseTheDebugger__ConsoleApp_
                 //or
                 x -= 3;
                 Console.WriteLine(x);
+               
             }
         }
     }
