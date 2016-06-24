9b1df70b9189b08c50f40d3dfcd80f6ab7ed6bc6
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-24 19:51:48 +0300

    Loops initial + fixed variables names

---
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/Loops.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseTheDebugger__ConsoleApp_
+{
+    class Loops
+    {
+        public static void Looping()
+        {
+            Console.WriteLine("Looping");
+            
+        }
+    }
+}
--- Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/Program.cs
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/Program.cs
@@ -10,32 +10,37 @@ namespace UseTheDebugger__ConsoleApp_
     {
         static void Main(string[] args)
         {
-            int n = 15; //15        
-            n = n + 10; //25
-            n = 36 * 10; //360
-            n = 12 - (42 / 7);//6
-            n += 10; //16
-            n *= 3; //48
-            n = 71 / 3; //23
+            //LearnToDebug();
+            Loops.Looping();
 
-            int c = 0;      Console.WriteLine(c); //0
-            c++;            Console.WriteLine(c); //1
-            c--;            Console.WriteLine(c); //0
+            //--------Delay--------
+            Console.ReadKey();
+        }
 
-            string r = "hello";
-            Console.WriteLine(r+=" again "+r); //hello again hello
-            Console.WriteLine(r = "the value of counter is: " + c);//the value of counter is: 0
-            Console.WriteLine(r="");//
+        private static void LearnToDebug()
+        {
+            int number = 15; //15        
+            number = number + 10; //25
+            number = 36 * 10; //360
+            number = 12 - (42 / 7);//6
+            number += 10; //16
+            number *= 3; //48
+            number = 71 / 3; //23
+
+            int count = 0; Console.WriteLine(count); //0
+            count++; Console.WriteLine(count); //1
+            count--; Console.WriteLine(count); //0
+
+            string result = "hello";
+            Console.WriteLine(result += " again " + result); //hello again hello
+            Console.WriteLine(result = "the value of counter is: " + count);//the value of counter is: 0
+            Console.WriteLine(result = "");//
 
             bool yesNo = false;
             bool anotherBool = true;
             yesNo = !anotherBool;
             Console.WriteLine(yesNo);//false
             Console.WriteLine(anotherBool);//true
-
-            //--------Delay--------
-            Console.ReadKey();
         }
-
     }
 }
