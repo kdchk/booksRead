b216906bd89cf07fdfa45e75aedbd535917f1fb3
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-25 10:55:39 +0300

    IfElse initial

---
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/if-else-statements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseTheDebugger__ConsoleApp_
+{
+    class if_else_statements
+    {
+        public static void IfElse()
+        {
+
+        }
+    }
+}
