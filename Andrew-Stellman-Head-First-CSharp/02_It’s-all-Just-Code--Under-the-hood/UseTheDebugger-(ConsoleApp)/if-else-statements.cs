36fb054e11c4e7417a12cfa35911f45cf4b91e79
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-25 11:25:41 +0300

    WinAppProject initial commit

--- Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/if-else-statements.cs
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/UseTheDebugger-(ConsoleApp)/if-else-statements.cs
@@ -17,17 +17,17 @@ namespace UseTheDebugger__ConsoleApp_
 
         private static void TryIfElse()
         {
-            string message = "";
+            string message = "Enter some int type value: ";
             int someValue = 0;
-            Console.Write("Enter some int type value: ");
+            Console.Write(message);
             someValue = Int32.Parse(Console.ReadLine());
             if (someValue == 24)
             {
-                Console.WriteLine(message="Value is 24. Yeah!");
+                Console.WriteLine(message = "Value is 24. Yeah!");
             }
             else
             {
-                Console.WriteLine(message="Value is NOT 24. =(");
+                Console.WriteLine(message = "Value is NOT 24. =(");
             }
         }
     }
