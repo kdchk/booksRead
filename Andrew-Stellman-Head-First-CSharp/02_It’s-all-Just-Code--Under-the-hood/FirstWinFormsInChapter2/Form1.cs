c4164724085f494007913f2559d850abae09ec15
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-25 20:16:14 +0300

    add changeText_Click method

--- Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/FirstWinFormsInChapter2/Form1.cs
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/FirstWinFormsInChapter2/Form1.cs
@@ -19,7 +19,26 @@ namespace FirstWinFormsInChapter2
 
         private void changeText_Click(object sender, EventArgs e)
         {
+            if (enableCheckbox.Checked == true)
+            {
+                if (labelToChange.Text == "Right")
+                {
+                    labelToChange.Text = "Left";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Right";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
 
+                }
+            }
+            else
+            {
+                labelToChange.Text = "Text changing is disabled";
+                labelToChange.TextAlign = ContentAlignment.MiddleCenter;
+
+            }
         }
     }
 }
