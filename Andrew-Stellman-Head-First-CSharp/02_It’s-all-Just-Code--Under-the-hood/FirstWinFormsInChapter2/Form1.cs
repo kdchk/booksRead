a4a56ab3b7af9f162dd5913fe86073bbdb01592c
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-25 20:11:16 +0300

    add Form design

--- Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/FirstWinFormsInChapter2/Form1.cs
+++ Andrew-Stellman-Head-First-CSharp/02_It’s-all-Just-Code--Under-the-hood/FirstWinFormsInChapter2/Form1.cs
@@ -16,5 +16,10 @@ namespace FirstWinFormsInChapter2
         {
             InitializeComponent();
         }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
