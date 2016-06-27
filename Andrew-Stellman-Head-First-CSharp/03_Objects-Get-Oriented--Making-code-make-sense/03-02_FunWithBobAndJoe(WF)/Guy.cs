7558e34d831552d83380f661afb3f0f57194dfce
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-27 22:32:53 +0300

    [i] 03_02_FunWithBobAndJoe_WF
    + add class Guy into it

---
+++ Andrew-Stellman-Head-First-CSharp/03_Objects-Get-Oriented--Making-code-make-sense/03-02_FunWithBobAndJoe(WF)/Guy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_02_FunWithBobAndJoe_WF_
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("У меня не хватает денег " + amount, Name + " говорит...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount+" мне нужно", Name + " говорит...");
+                return 0;
+            }
+        }
+    }
+}
