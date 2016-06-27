8f7c62894b7f2bde3cb04868fec11ee17aefb951
kdchk-Skylake <kdchk.net@gmail.com>
2016-06-27 23:00:37 +0300

    [w]  03_02_FunWithBobAndJoe_WF_
    work as in the book

--- Andrew-Stellman-Head-First-CSharp/03_Objects-Get-Oriented--Making-code-make-sense/03-02_FunWithBobAndJoe(WF)/Form1.Designer.cs
+++ Andrew-Stellman-Head-First-CSharp/03_Objects-Get-Oriented--Making-code-make-sense/03-02_FunWithBobAndJoe(WF)/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.joesCashLabel = new System.Windows.Forms.Label();
+            this.bobsCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // joesCashLabel
+            // 
+            this.joesCashLabel.AutoSize = true;
+            this.joesCashLabel.Location = new System.Drawing.Point(12, 9);
+            this.joesCashLabel.Name = "joesCashLabel";
+            this.joesCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.joesCashLabel.TabIndex = 0;
+            this.joesCashLabel.Text = "label1";
+            // 
+            // bobsCashLabel
+            // 
+            this.bobsCashLabel.AutoSize = true;
+            this.bobsCashLabel.Location = new System.Drawing.Point(12, 29);
+            this.bobsCashLabel.Name = "bobsCashLabel";
+            this.bobsCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobsCashLabel.TabIndex = 1;
+            this.bobsCashLabel.Text = "label2";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(12, 49);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give $10 to Joe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Receive $5 from Bob";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 136);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobsCashLabel);
+            this.Controls.Add(this.joesCashLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label joesCashLabel;
+        private System.Windows.Forms.Label bobsCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Andrew-Stellman-Head-First-CSharp/03_Objects-Get-Oriented--Making-code-make-sense/03-02_FunWithBobAndJoe(WF)/Form1.cs
+++ Andrew-Stellman-Head-First-CSharp/03_Objects-Get-Oriented--Making-code-make-sense/03-02_FunWithBobAndJoe(WF)/Form1.cs
@@ -12,9 +12,48 @@ namespace _03_02_FunWithBobAndJoe_WF_
 {
     public partial class Form1 : Form
     {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+
         public Form1()
         {
             InitializeComponent();
+            joe = new Guy();
+            joe.Name="Joe";
+            joe.Cash = 50;
+
+            bob = new Guy();
+            bob.Name = "Bob";
+            bob.Cash = 100;
+            UpdateForm();
+        }
+
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("В банке нет денег");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
         }
     }
 }
