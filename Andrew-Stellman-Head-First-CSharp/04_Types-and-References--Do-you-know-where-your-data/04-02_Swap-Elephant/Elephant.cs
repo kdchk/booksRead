5e4fdcf2eaff67cf648b3b2974424d98106ebbe2
kdchk-Skylake <kdchk.net@gmail.com>
2016-07-10 21:46:46 +0300

    [w] my variant of 04_02_Swap_Elephant

--- Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-02_Swap-Elephant/Elephant.cs
+++ Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-02_Swap-Elephant/Elephant.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _04_02_Swap_Elephant
 {
     class Elephant
     {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show($"My ears are {EarSize} tall", $"{Name} says...");
+        }
     }
 }
--- Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-02_Swap-Elephant/Form1.cs
+++ Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-02_Swap-Elephant/Form1.cs
@@ -12,9 +12,31 @@ namespace _04_02_Swap_Elephant
 {
     public partial class Form1 : Form
     {
+        Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant swapTemp = lucinda;
+            lucinda = lloyd;
+            lloyd = swapTemp;
+            MessageBox.Show("Objects swapped", "Swap!");
         }
     }
 }
