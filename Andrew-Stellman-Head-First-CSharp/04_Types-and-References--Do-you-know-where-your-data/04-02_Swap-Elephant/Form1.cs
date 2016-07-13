eb33df2836d770afeea4db2e89b3ba22af8f5595
kdchk-Skylake <kdchk.net@gmail.com>
2016-07-14 01:33:33 +0300

    [m] add biggestEars to 04_02_Swap_Elephant

--- Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-02_Swap-Elephant/Form1.cs
+++ Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-02_Swap-Elephant/Form1.cs
@@ -44,5 +44,27 @@ namespace _04_02_Swap_Elephant
             MessageBox.Show("Objects swapped", "Swap!");
         }
 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Elephant[] elephants = new Elephant[7];
+            elephants[0] = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            elephants[1] = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            elephants[2] = new Elephant() { Name = "Larry", EarSize = 42 };
+            elephants[3] = new Elephant() { Name = "Lucille", EarSize = 32 };
+            elephants[4] = new Elephant() { Name = "Lars", EarSize = 44 };
+            elephants[5] = new Elephant() { Name = "Linda", EarSize = 37 };
+            elephants[6] = new Elephant() { Name = "Humphrey", EarSize = 45 };
+
+            Elephant biggestEars = elephants[0];
+
+            for (int i = 1; i < elephants.Length; i++)
+            {
+                if (elephants[i].EarSize > biggestEars.EarSize)
+                {
+                    biggestEars = elephants[i];
+                }
+            }
+            MessageBox.Show(biggestEars.EarSize.ToString(), biggestEars.Name.ToString());
+        }
     }
 }
