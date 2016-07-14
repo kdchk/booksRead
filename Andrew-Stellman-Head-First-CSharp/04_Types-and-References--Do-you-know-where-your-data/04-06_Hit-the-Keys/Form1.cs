01e6b15a05a5f474acc21ed19e090ca33d49ace4
kdchk-Skylake <kdchk.net@gmail.com>
2016-07-15 01:52:23 +0300

    [i] 04_06_Hit_the_Keys initial + timer1_Tick

---
+++ Andrew-Stellman-Head-First-CSharp/04_Types-and-References--Do-you-know-where-your-data/04-06_Hit-the-Keys/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_06_Hit_the_Keys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 50));
+            if (listBox1.Items.Count>7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over!");
+                timer1.Stop();
+            }
+        }
+    }
+}
