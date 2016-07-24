8f480787496e3c13da6c321b49aebbc06aaa3e6d
kdchk-Skylake <kdchk.net@gmail.com>
2016-07-24 14:06:57 +0300

    [m] 05_02_Cow_calculator - incapsulate class Farmer

--- Andrew-Stellman-Head-First-CSharp/05_Encapsulation--Keep-your-privates-Private/05-02_Cow-calculator/Farmer.cs
+++ Andrew-Stellman-Head-First-CSharp/05_Encapsulation--Keep-your-privates-Private/05-02_Cow-calculator/Farmer.cs
@@ -8,8 +8,16 @@ namespace _05_02_Cow_calculator
 {
     class Farmer
     {
-        public int BagsOfFeed;
-        public const int FeedMultiplier = 30;
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+
+        public int BagsOfFeed { get; private set; }
+
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }
 
         private int numberOfCows;
         public int NumberOfCows
--- Andrew-Stellman-Head-First-CSharp/05_Encapsulation--Keep-your-privates-Private/05-02_Cow-calculator/Form1.cs
+++ Andrew-Stellman-Head-First-CSharp/05_Encapsulation--Keep-your-privates-Private/05-02_Cow-calculator/Form1.cs
@@ -16,7 +16,7 @@ namespace _05_02_Cow_calculator
         public Form1()
         {
             InitializeComponent();
-            farmer = new Farmer() { NumberOfCows = 15 };
+            farmer = new Farmer(15, 30);
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
