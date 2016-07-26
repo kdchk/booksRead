1b6b8283be854640dffb87199c80ec6a0a8c8d92
kdchk-Skylake <kdchk.net@gmail.com>
2016-07-26 17:26:45 +0300

    [w] 05_06_BaseClass_Constructor
    almost as in the book

--- Andrew-Stellman-Head-First-CSharp/05_Encapsulation--Keep-your-privates-Private/05-06_BaseClass-Constructor/MyBaseClass.cs
+++ Andrew-Stellman-Head-First-CSharp/05_Encapsulation--Keep-your-privates-Private/05-06_BaseClass-Constructor/MyBaseClass.cs
@@ -3,7 +3,38 @@
 
 namespace _05_06_BaseClass_Constructor
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyBaseClass myBase = new MyBaseClass("Hello, I'm your base class-[baseClassNeedsThis]");
+            /*Output: 
+            This is the base class: Hello, I'm your base class-[baseClassNeedsThis]
+            */
+
+            MySubClass mySub = new MySubClass("Hello, I'm your subclass class-[baseClassNeedsThis]", 42);
+            /*Output:
+            This is the base class: Hello, I'm your subclass class-[baseClassNeedsThis]
+            then another MessageBox
+            This is the subclass: Hello, I'm your subclass class-[baseClassNeedsThis] and 42
+            */
+        }
+    }
     class MyBaseClass
     {
+        public MyBaseClass(string baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the base class: " + baseClassNeedsThis);
+        }
+    }
+
+    class MySubClass : MyBaseClass
+    {
+        public MySubClass(string baseClassNeedsThis, int anotherValue)
+            : base(baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the subclass: " + baseClassNeedsThis + " and " + anotherValue);
+        }
     }
 }
+
